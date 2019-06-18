cf619ba75c714ca10a1279b824cc3783eac363e2
BogdanZeleniuk <bottan@ukr.net>
2019-06-18 21:44:46 +0200

    update functions

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@ function Calendar(options) {
 			input_date.className = 'datetime';
 
 			var currentDate = new Date();
-			var day = currentDate.getDate();
+			var day = currentDate.getDate() < 10 ? ('0' + currentDate.getMonth()) : 
+											currentDate.getDate();
 			var month = (currentDate.getMonth()+1) < 10 ? 
 								('0' + (currentDate.getMonth()+1)) : (currentDate.getMonth()+1);
 			var year = currentDate.getFullYear();
@@ -116,7 +117,7 @@ function Search(options) {
 		elem.firstChild.style.borderColor = 'green';
 		elem.firstChild.style.borderRadius = 10 + 'px';
 		document.addEventListener('click', outsideInputClick);
-   		isOpen = true;
+   		isOpen = true;	
 	}
 
 	function outsideInputClick(event) {
@@ -265,4 +266,62 @@ function Picture(options){
 			}
 		}
 	}		
+}
+function Arrow(options){
+	var elem_left = options.elem_left;
+	var elem_right = options.elem_right;
+
+	elem_right.onclick = function(event){
+		var postList = document.getElementsByClassName('post');
+		for (var i = 0; i < postList.length; i++) {
+			if (postList[i].querySelector('.post_head.post_head img').classList.contains('post_active_img') &&
+					i < 2) {
+				postList[i].querySelector('.post_head.post_head img').classList.remove('post_active_img');
+				postList[i].querySelector('.post_head > .day').classList.remove('day_active');
+				postList[i].querySelector('.post_head > .month').classList.remove('month_active');
+
+				postList[i].querySelector('.post-desc').classList.remove('post_active');
+				postList[i].querySelector('.post-desc > .title_post_desc').classList.remove('post_desc_title_active');
+				postList[i].querySelector('.post-desc > .post_desc_text').classList.remove('post_desc_text_active');
+
+				postList[i+1].querySelector('.post_head.post_head img').classList.add('post_active_img');
+				postList[i+1].querySelector('.post_head > .day').classList.add('day_active');
+				postList[i+1].querySelector('.post_head > .month').classList.add('month_active');
+
+				postList[i+1].querySelector('.post-desc').classList.add('post_active');
+				postList[i+1].querySelector('.post-desc > .title_post_desc').classList.add('post_desc_title_active');
+				postList[i+1].querySelector('.post-desc > .post_desc_text').classList.add('post_desc_text_active');
+			}
+			else if (i < 2) {
+				elem_right.style.backgroundImage = "url('../waxom_web/img/left_posts.png')";
+				elem_right.style.transform = 'scale(-1,1)';
+				elem_left.style.backgroundImage = "url('../waxom_web/img/right_posts.png')";
+				elem_left.style.transform = 'scale(-1,1)';
+			}
+		}
+	}
+
+	elem_left.onclick = function(event){
+		var postList = document.getElementsByClassName('post');
+		for (var i = 0; i < postList.length; i++) {
+			if (postList[i].querySelector('.post_head.post_head img').classList.contains('post_active_img') &&
+					i > 0) {
+				postList[i].querySelector('.post_head.post_head img').classList.remove('post_active_img');
+				postList[i].querySelector('.post_head > .day').classList.remove('day_active');
+				postList[i].querySelector('.post_head > .month').classList.remove('month_active');
+
+				postList[i].querySelector('.post-desc').classList.remove('post_active');
+				postList[i].querySelector('.post-desc > .title_post_desc').classList.remove('post_desc_title_active');
+				postList[i].querySelector('.post-desc > .post_desc_text').classList.remove('post_desc_text_active');
+
+				postList[i-1].querySelector('.post_head.post_head img').classList.add('post_active_img');
+				postList[i-1].querySelector('.post_head > .day').classList.add('day_active');
+				postList[i-1].querySelector('.post_head > .month').classList.add('month_active');
+
+				postList[i-1].querySelector('.post-desc').classList.add('post_active');
+				postList[i-1].querySelector('.post-desc > .title_post_desc').classList.add('post_desc_title_active');
+				postList[i-1].querySelector('.post-desc > .post_desc_text').classList.add('post_desc_text_active');
+			}
+		}
+	}
 }
